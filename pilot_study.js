bebe10c344ae86e34a049d9b7a8646c75c9dce53
916046794 <tuo74327@temple.edu>
2025-07-02 14:46:37 -0400

    WIP GUI appearance

--- pilot_study.js
+++ pilot_study.js
@@ -110,14 +110,14 @@ const psychoJS = new PsychoJS({
 });
 
 // open window:
-/*psychoJS.openWindow({
+psychoJS.openWindow({
   fullscr: false,
   color: new util.Color([0,0,0]),
   units: 'height',
   waitBlanking: true,
   backgroundImage: '',
   backgroundFit: 'none',
-});*/
+});
 
 //the below is commented out for debugging: ctrl+f DEBUG to see all comments
 //a lot of below can be removed. This schedule() is what calls DlgFromDict() to create a confirmation thing from user
@@ -134,7 +134,7 @@ const flowScheduler = new Scheduler(psychoJS);
 const dialogCancelScheduler = new Scheduler(psychoJS);
 psychoJS.scheduleCondition(function() { return (psychoJS.gui.dialogComponent.button === 'OK'); },flowScheduler, dialogCancelScheduler);
 */
-psychoJS.add(flowScheduler);
+psychoJS.schedule(flowScheduler);
 
 // flowScheduler gets run if the participants presses OK
 flowScheduler.add(updateInfo); // add timeStamp
